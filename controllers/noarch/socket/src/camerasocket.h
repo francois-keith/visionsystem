f3859fe59017ce9f298a30fc7ab68e1a23b7b761
Pierre Gergondet <pierre.gergondet@aist.go.jp>
2011-07-07 18:40:13 +0900

    Augment buffer size to reduce lag

--- controllers/noarch/socket/src/camerasocket.h
+++ controllers/noarch/socket/src/camerasocket.h
@@ -71,7 +71,7 @@ private:
     udp::endpoint receiver_endpoint_;
     udp::endpoint sender_endpoint_;
     std::string request_;
-    enum { chunk_size_ = 2049 };
+    enum { chunk_size_ = 8193 };
     unsigned char chunk_buffer_[chunk_size_];
     uint8_t chunkID_;    
 
