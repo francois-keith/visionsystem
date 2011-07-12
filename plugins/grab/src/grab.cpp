0e91e735b778036d98b8c1ee769a1927c7493294
Druon Sebastien <seb@homedruon.com>
2011-07-12 18:37:41 +0900

    mmmhh ... po compris

--- plugins/grab/src/grab.cpp
+++ plugins/grab/src/grab.cpp
@@ -1,11 +1,13 @@
 #include "grab.h"
 
+#include <vision/io/imageio.h>
 #include <iostream>
 
 Grab::Grab( VisionSystem *vs, string sandbox )
 :Plugin( vs, "grab", sandbox ), WithViewer( vs )  
 {
-
+	grab_next = false ;
+	grabbed.clear() ;
 }
 
 Grab::~Grab() {
@@ -18,8 +20,23 @@ bool Grab::pre_fct() {
 	try {
 		read_config_file( filename.c_str() ) ; 
 	} catch ( string msg ) {
+		
 		cout << "[grab] Could not read config file" << endl ;
-		cout << "[grab] Using default settings" << endl ;
+		cout << "[grab] Will grab all active cameras" << endl ;
+
+		vector<Camera*> cams = get_all_cameras() ;
+
+		for (int i=0; i<cams.size(); i++ ) {
+			if ( cams[i]->is_active() ) {
+				Registration* newreg = new Registration() ;
+				newreg->camera = cams[i] ;
+				newreg->grabbed_frames.clear() ;
+				grabbed.push_back( newreg ) ;
+				cout << "[grab] will grab on " << cams[i]->get_name() << endl ;
+			}
+		}
+
+
 	}
 
 	Viewer* vw = whiteboard_read< Viewer* >( string("viewer")) ;
@@ -30,20 +47,48 @@ bool Grab::pre_fct() {
 	}
 	
 	register_callback() ;
-	
+
 	return true ;
 }
 
 void Grab::preloop_fct() {
 
+	for ( int i=0; i<grabbed.size(); i++ )
+		register_to_cam< Image<uint32_t, RGB> > ( grabbed[i]->camera, 50 ) ;
 
 }
 
 void Grab::loop_fct() {
 
+	for ( int i=0; i<grabbed.size(); i++ )
+		grabbed[i]->current_frame = dequeue_image< Image<uint32_t, RGB> > ( grabbed[i]->camera ) ;
+
+
+	if ( grab_next ) {
+
+		for ( int i=0; i<grabbed.size(); i++ )
+			grabbed[i]->grabbed_frames.push_back( grabbed[i]->current_frame ) ;
+
+		grab_next = false ;
+
+	} else {
+
+		for ( int i=0; i<grabbed.size(); i++ )
+			enqueue_image< Image<uint32_t, RGB> > ( grabbed[i]->camera, grabbed[i]->current_frame ) ;
+	}
+
 }
 
 bool Grab::post_fct() {
+
+	for ( int i=0; i<grabbed.size(); i++ )
+	for ( int j=0; j<grabbed[i]->grabbed_frames.size(); j++ ) {
+
+		std::ostringstream filename;
+		filename << get_sandbox() << "/" << grabbed[i]->camera->get_name() << "-" << j << ".png" ;  
+		save_color < uint32_t, RGB > ( filename.str() , grabbed[i]->current_frame ) ;
+	}
+
 	return true ;
 }
 
@@ -62,6 +107,7 @@ void Grab::callback(Camera* cam, XEvent event) {
 
 			case XK_g:
 				cout << "[grab] Grabing ... " << endl ;
+				grab_next = true ;
 				break;
 			
 			default:
--- plugins/grab/src/grab.h
+++ plugins/grab/src/grab.h
@@ -1,11 +1,13 @@
 #ifndef GRAB_H
 #define GRAB_H
 
+#include <vision/image/image.h>
 #include <visionsystem/plugin.h>
 #include <visionsystem/viewer.h>
 #include <configparser/configparser.h>
 
 using namespace std ;
+using namespace vision ;
 using namespace visionsystem ;
 using namespace configparser ;
 
@@ -27,6 +29,20 @@ class Grab : public Plugin, public WithViewer, public WithConfigFile {
 		void glfunc( Camera* cam ) ;
 
 		void parse_config_line( vector<string> &line ) ;
+
+
+		class Registration {
+
+			public:
+
+			Camera* camera ;
+			Image<uint32_t, RGB> *current_frame ;
+			vector< Image< uint32_t, RGB >* > grabbed_frames ;
+		} ;
+
+
+		bool grab_next ;			// should next frames be grabbed ?
+		vector< Registration* >	grabbed ;
 
 } ;
 
